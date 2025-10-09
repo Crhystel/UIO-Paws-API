db758760b0436befc727b073bf5b07e7e4a5946e
Kpiiale <crhystelvelasco213@gmail.com>
2025-10-09 15:00:20 -0500

    creating safe admin on database

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@adopcion.com',
+            'password' => Hash::make('UioPaws123'),
+            'role' => 'admin',
+        ]);
+    }
+}
