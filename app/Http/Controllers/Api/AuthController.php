26e010fcfa7099767bf9e2d8d254aeed4bf08f3a
Kpiiale <crhystelvelasco213@gmail.com>
2025-10-09 19:29:42 -0500

    fixing errors with middlewares

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,13 +14,14 @@
             $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
-                'password' => 'required|string|min:8|confirmed',
+                'password' => 'required|string|min:8',
             ]);
 
             User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
+                'role' => 'user',
             ]);
 
             return response()->json(['message' => 'Usuario registrado exitosamente.'], 201);
