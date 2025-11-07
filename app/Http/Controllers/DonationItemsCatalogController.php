0ae673a0cf774f4f3b9c08f16c601c518f85d78d
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-07 16:13:29 -0500

    completed crud for items on donations catalog

---
+++ app/Http/Controllers/DonationItemsCatalogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Controllers\Controller;
+use App\Models\DonationItemsCatalog;
+
+class DonationItemsCatalogController extends Controller
+{
+    public function index(){
+        return DonationItemsCatalog::all();
+    }
+    public function store(Request $request){
+        $validated=$request->validate([
+            'item-name'=>'required|string|unique:donation_items_catalog|max:255',
+            'description'=>'nullable|string',
+        ]);
+        $item=DonationItemsCatalog::create($validated);
+        return repsonse()->json($item,201);
+    }
+    public function show(DonationItemsCatalog $donationItemsCatalog){
+        return $donationItemsCatalog;
+    }
+    public function update(Request $request,DonationItemsCatalog $donationItemsCatalog){
+        $validated=$request->validate([
+            'item_name'=>'sometimes|required|string|max:255|unique:donation_items_catalog,item_name,'.$donationItemsCatalog->id_donation_item_catalog.',id_donation_item_catalog',
+            'description'=>'sometimes|nullable|string',
+        ]);
+        $donationItemsCatalog->update($validated);
+        return response()->json($donationItemsCatalog);
+    }
+    public function destroy(DonationItemsCatalog $donationItemsCatalog){
+        $donationItemsCatalog->delete();
+        return response()->json(null,204);
+    }
+}
