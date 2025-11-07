ad6aed5e5ebc7c4089113f28c841b01335d436a5
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-07 15:42:57 -0500

    crud on species controller done

---
+++ app/Http/Controllers/SpeciesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Controllers\Controller;
+use App\Models\Species;
+
+class SpeciesController extends Controller
+{
+    public function index(){
+        return Species::orderBy('species_name')->get();
+    }
+    public function store(Request $request){
+        $validated=$request->validate([
+            'species_name'=>'required|string|unique:species|max:255'
+        ]);
+        $species=Species::create($validated);
+        return response()->json($species,201);
+    }
+    public function show(Species $species){
+        return $species;
+    }
+    public function update(Request $request, Species $species){
+        $validated=$request->validate([
+            'species_name'=>'required|string|unique:species,species_name,'.$species->id_species.',id_species|max:255'
+        ]);
+        $species->update($validated);
+        return response()->json($species);
+    }
+    public function destroy(Species $species){
+        $species->delete();
+        return response()->json($species,204);
+    }
+}
