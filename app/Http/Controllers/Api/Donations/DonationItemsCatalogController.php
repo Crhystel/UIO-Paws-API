c4804f8169ecd77f6397aa246da284b22af3fbb7
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-09 00:40:50 -0500

    rearranging controller files

--- app/Http/Controllers/Api/Donations/DonationItemsCatalogController.php
+++ app/Http/Controllers/Api/Donations/DonationItemsCatalogController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\Donations;
 
 use Illuminate\Http\Request;
 use App\Controllers\Controller;
--- app/Http/Controllers/Api/Login/AuthController.php
+++ app/Http/Controllers/Api/Login/AuthController.php
@@ -1,5 +1,5 @@
 <?php
-    namespace App\Http\Controllers\Api;
+    namespace App\Http\Controllers\Api\Login;
 
     use App\Http\Controllers\Controller;
     use Illuminate\Http\Request;
