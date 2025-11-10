beb795b83d40bd3307d6f25abd8b7e2b5f78776d
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-10 18:28:30 -0500

    adding other role, changing info on seeders and authentication on controllers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,44 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash; 
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
         $this->call([
-            RolesAndPermissionsSeeder::class,
-            ApplicationStatusSeeder::class,
-            AdminUserSeeder::class,
+            RoleAndPermissionSeeder::class, 
+            ApplicationStatusSeeder::class, 
         ]);
+        $superAdmin = User::create([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'email' => 'superadmin@adopcion.com',
+            'password_hash' => Hash::make('UioPawsSuperAdmin123'),
+            'is_active' => true,
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        $admin = User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Principal',
+            'email' => 'admin@adopcion.com',
+            'password_hash' => Hash::make('UioPawsAdmin123'),
+            'is_active' => true,
+        ]);
+        $admin->assignRole('Admin');
+        $user = User::create([
+            'first_name' => 'Usuario',
+            'last_name' => 'Ejemplo',
+            'email' => 'user@adopcion.com',
+            'password_hash' => Hash::make('UioPawsUser123'),
+            'is_active' => true,
+        ]);
+        $user->assignRole('User');
     }
 }
