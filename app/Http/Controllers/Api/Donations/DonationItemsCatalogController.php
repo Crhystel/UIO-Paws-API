bbd42b6dc8b9615314b5a9758a59272ec74dbc01
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-10 15:06:05 -0500

    fixing typos, variable name problems, and removing unnecessary files

--- app/Http/Controllers/Api/Donations/DonationItemsCatalogController.php
+++ app/Http/Controllers/Api/Donations/DonationItemsCatalogController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\Donations;
 
 use Illuminate\Http\Request;
-use App\Controllers\Controller;
+use App\Http\Controllers\Controller; 
 use App\Models\DonationItemsCatalog;
 
 class DonationItemsCatalogController extends Controller
@@ -12,26 +12,26 @@ class DonationItemsCatalogController extends Controller
         return DonationItemsCatalog::all();
     }
     public function store(Request $request){
-        $validated=$request->validate([
-            'item-name'=>'required|string|unique:donation_items_catalog|max:255',
-            'description'=>'nullable|string',
+        $validated = $request->validate([
+        'item_name' => 'required|string|unique:donation_items_catalog|max:255', 
+        'description' => 'nullable|string',
         ]);
-        $item=DonationItemsCatalog::create($validated);
-        return repsonse()->json($item,201);
+        $item = DonationItemsCatalog::create($validated);
+        return response()->json($item, 201); 
     }
     public function show(DonationItemsCatalog $donationItemsCatalog){
         return $donationItemsCatalog;
     }
-    public function update(Request $request,DonationItemsCatalog $donationItemsCatalog){
-        $validated=$request->validate([
-            'item_name'=>'sometimes|required|string|max:255|unique:donation_items_catalog,item_name,'.$donationItemsCatalog->id_donation_item_catalog.',id_donation_item_catalog',
-            'description'=>'sometimes|nullable|string',
+    public function update(Request $request, DonationItemsCatalog $donationItemsCatalog){
+        $validated = $request->validate([
+        'item_name' => 'sometimes|required|string|max:255|unique:donation_items_catalog,item_name,'.$donationItemsCatalog->id_donation_item_catalog.',id_donation_item_catalog',
+        'description' => 'sometimes|nullable|string',
         ]);
         $donationItemsCatalog->update($validated);
         return response()->json($donationItemsCatalog);
     }
     public function destroy(DonationItemsCatalog $donationItemsCatalog){
         $donationItemsCatalog->delete();
-        return response()->json(null,204);
+        return response()->json(null, 204); 
     }
 }
