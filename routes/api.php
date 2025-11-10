286031f9c607cde0d76478e3a7d9a537cd0f4645
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-10 18:28:30 -0500

    adding other role, changing info on seeders and authentication on controllers

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Api\Volunteers\VolunteerOpportunityController;
 use App\Http\Controllers\Api\User\ProfileController;
 use App\Http\Controllers\Api\Admin\ApplicationController as AdminApplicationController;
 
-// Apunta a Api/Login/AuthController
+// --- RUTAS PÚBLICAS Y DE AUTENTICACIÓN ---
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -38,7 +38,6 @@ Route::prefix('public')->name('public.')->group(function () {
 
 // --- RUTAS PARA USUARIOS AUTENTICADOS ---
 Route::middleware('auth:sanctum')->group(function () {
-    // Gestión de Sesión y Perfil (usa el AuthController de Login)
     Route::post('/logout', [AuthController::class, 'logout']);
     
     // RUTA PRINCIPAL DE PERFIL
