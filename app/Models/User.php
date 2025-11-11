34e87c43fad5590fe927cad03872ff04198f7142
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-11 01:08:14 -0500

    adding missing fields on user registration form

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,15 @@ class User extends Authenticatable
             'password_hash' => 'hashed',
         ];
     }
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password_hash;
+    }
     public function address()
     {
         return $this->belongsTo(Address::class, 'id_address');
