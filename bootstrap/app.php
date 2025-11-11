a58e8da24c177e7b1c58fe945911a6c49848d936
Kpiiale <crhystelvelasco213@gmail.com>
2025-11-11 14:20:11 -0500

    fixing innecesary double authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
-         $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
