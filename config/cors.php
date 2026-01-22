7c2dda51a5c6aba4f73889a685e129d57420ab97
Crhystel <crhystelvelasco213@gmail.com>
2026-01-22 04:22:21 -0500

    adding allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8001,http://127.0.0.1:8001')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8001,http://127.0.0.1:8001,https://lovable.dev/projects/dc67336a-fe18-47bd-a68e-ea94e66647f9?view=design')),
 
     'allowed_origins_patterns' => [],
 
